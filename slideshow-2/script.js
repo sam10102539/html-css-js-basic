6c436d78c75a32cb22403bece7ee2af1f5e11dcc
sam10102539 <sam.n.k@hotmail.com>
2023-12-10 21:05:33 +0700

    enhance slideshow-2 for auto slideshow

--- slideshow-2/script.js
+++ slideshow-2/script.js
@@ -1,15 +1,25 @@
+// automatic slideshow
+function autoSlide() {
+        let lists = document.querySelectorAll('.item');
+        document.getElementById('slide').appendChild(lists[0]); 
+}
+
+let autoSlideInterval = setInterval(autoSlide, 4000);
+
+function resetSlideInterval(){
+    clearInterval(autoSlideInterval)
+    autoSlideInterval = setInterval(autoSlide, 4000);
+}
+
 document.getElementById('next').onclick = function(){
     let lists = document.querySelectorAll('.item');
     document.getElementById('slide').appendChild(lists[0]);
+    resetSlideInterval();
 }
 
 document.getElementById('prev').onclick = function(){
     let lists = document.querySelectorAll('.item');
     document.getElementById('slide').prepend(lists[lists.length - 1]);
+    resetSlideInterval();
 }
 
-// 
-setInterval(function() {
-    let lists = document.querySelectorAll('.item');
-    document.getElementById('slide').appendChild(lists[0]);
-}, 4500);
